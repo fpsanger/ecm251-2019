892c6441a394ab5d920713acf878ceadef1e31e4
FELIPE  PEREIRA SANGER <17.02225-8@maua.br>
2019-03-11 11:36:35 -0300

    Projeto criado com Netbeans

---
+++ Pratica_01/meuPrimeiroProjetoComNetbeans/src/UsandoNetbeans.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 17.02225-8
+ */
+public class UsandoNetbeans {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+    }
+}
