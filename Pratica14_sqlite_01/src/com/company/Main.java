19606d353b7dade0fefac870e8b46e5029e97d3c
FELIPE  PEREIRA SANGER <17.02225-8@maua.br>
2019-08-26 13:24:46 -0300

    Pratica 14-banco

---
+++ Pratica14_sqlite_01/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import auxiliar_database.UsuariosDAO;
+import model.Usuario;
+import utilities.Constants;
+
+import java.util.List;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Aplicacao app = new Aplicacao();
+        app.run();
+
+
+        }
+    }
+
+
